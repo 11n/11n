a9fcb4d33870defd8bc9f603f89cebc64095fc88
Yair <hello@agzeri.com>
2017-12-09 23:11:28 -0600

    [tool] Update Test Suite and update dependencies.

---
+++ specs/pkg.spec.js
@@ -0,0 +1,5 @@
+import pkg from '../src/pkg';
+
+test('Hethe, Motion Design Framework', () => {
+  expect(pkg.name).toBe('hethe');
+});
--- src/11n.spec.js
+++
@@ -1,5 +0,0 @@
-import sayHello from './11n';
-
-test('11n says hello', () => {
-  expect(sayHello('11n')).toBe('11n says hello');
-});
